63dd2703e0f088386001c9be8547e85ef0a9e308
hongchuan.du <hongchuan.du@DHC.bizapper.com>
2015-01-26 17:56:37 +0800

    修改菜單

--- src/com/dhonchuan/learningproject/activity/CustomMenuActivity.java
+++ src/com/dhonchuan/learningproject/activity/CustomMenuActivity.java
@@ -1,9 +1,13 @@
 package com.dhonchuan.learningproject.activity;
 
-import com.example.learningproject.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.dhongchuan.learningproject.customview.MenuLayout;
+import com.example.learningproject.R;
 
 public class CustomMenuActivity extends Activity{
 
@@ -12,6 +16,17 @@ public class CustomMenuActivity extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_custom_menu);
+		Button button = (Button) findViewById(R.id.button);
+		final MenuLayout menu = (MenuLayout) findViewById(R.id.menu);
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				menu.menuToggle();
+			}
+		});
+		
 	}
 
 }
--- src/com/dhongchuan/learningproject/customview/MenuLayout.java
+++ src/com/dhongchuan/learningproject/customview/MenuLayout.java
@@ -14,18 +14,19 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.dhongchuan.learingproject.util.AnimatorUtils;
+import com.dhongchuan.learingproject.util.Utils;
 import com.example.learningproject.R;
 
-public class MenuLayout extends RelativeLayout {
+public class MenuLayout extends LinearLayout {
 	private boolean mIsMenuOpen;
 	private boolean mIsAnimationRun;
 	private Context mContext;
 	private int menuItemSize;
-	
-	private AnimatorSet mAnimatorSetHideMenu;
-    private AnimatorSet mAnimatorSetShowMenu;
 
-    
+	private AnimatorSet mAnimatorSetHideMenu;
+	private AnimatorSet mAnimatorSetShowMenu;
+
 	public MenuLayout(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		// TODO Auto-generated constructor stub
@@ -43,120 +44,119 @@ public class MenuLayout extends RelativeLayout {
 		// TODO Auto-generated constructor stub
 		initView();
 	}
-	
+
 	public void menuToggle() {
-        if (!mIsAnimationRun) {
-            resetAnimations();
-            mIsAnimationRun = true;
-            if (mIsMenuOpen) {
-                mAnimatorSetHideMenu.start();
-            } else {
-                mAnimatorSetShowMenu.start();
-            }
-            toggleIsMenuOpen();
-        }
-    }
-	
-	private void initView(){
+		if (!mIsAnimationRun) {
+			resetAnimations();
+			mIsAnimationRun = true;
+			if (mIsMenuOpen) {
+				mAnimatorSetHideMenu.start();
+			} else {
+				mAnimatorSetShowMenu.start();
+			}
+			toggleIsMenuOpen();
+		}
+	}
+
+	private void toggleIsMenuOpen() {
+		mIsMenuOpen = !mIsMenuOpen;
+	}
+
+	private void initView() {
+		setOrientation(LinearLayout.VERTICAL);
 		mContext = getContext();
 		menuItemSize = 40;
 		mIsMenuOpen = false;
 		mIsAnimationRun = false;
-		LinearLayout.LayoutParams imageWrapperLayoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		for(int i = 0; i < 5; i++){
-			addView(getImageWrapper());
+		LinearLayout.LayoutParams imageWrapperLayoutParams = new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        ArrayList<Integer> id = new ArrayList<>();
+        id.add(R.drawable.icn_1);
+        id.add(R.drawable.icn_2);
+        id.add(R.drawable.icn_3);
+        id.add(R.drawable.icn_4);
+        id.add(R.drawable.icn_5);
+		for (int i = 0; i < 5; i++) {
+			addView(Utils.getImageWrapper(mContext, 80, id.get(i), null), imageWrapperLayoutParams);
 		}
-		resetAnimations();
+//		resetAnimations();
 		mAnimatorSetShowMenu = setOpenCloseAnimation(false);
-        mAnimatorSetHideMenu = setOpenCloseAnimation(true);
+		mAnimatorSetHideMenu = setOpenCloseAnimation(true);
 
 	}
-	
-	 private AnimatorSet setOpenCloseAnimation(boolean isCloseAnimation) {
-	        List<Animator> textAnimations = new ArrayList<Animator>();
-	        List<Animator> imageAnimations = new ArrayList<Animator>();
 
-	        if (isCloseAnimation) {
-	            for (int i = getChildCount() - 1; i >= 0; i--) {
-	                fillOpenClosingAnimations(true, textAnimations, imageAnimations, i);
-	            }
-	        } else {
-	            for (int i = 0; i < getChildCount(); i++) {
-	                fillOpenClosingAnimations(false, textAnimations, imageAnimations, i);
-	            }
-	        }
+	private AnimatorSet setOpenCloseAnimation(boolean isCloseAnimation) {
+		List<Animator> imageAnimations = new ArrayList<Animator>();
+		if (isCloseAnimation) {
+			for (int i = getChildCount() - 1; i >= 0; i--) {
+				fillCloseAnimations(imageAnimations, i);
+			}
+		} else {
+			for (int i = 0; i < getChildCount(); i++) {
+				fillOpenAnimations(imageAnimations, i);
+			}
+		}
 
-	        AnimatorSet textCloseAnimatorSet = new AnimatorSet();
-	        textCloseAnimatorSet.playSequentially(textAnimations);
+		AnimatorSet imageCloseAnimatorSet = new AnimatorSet();
+		imageCloseAnimatorSet.playSequentially(imageAnimations);
 
-	        AnimatorSet imageCloseAnimatorSet = new AnimatorSet();
-	        imageCloseAnimatorSet.playSequentially(imageAnimations);
-
-	        AnimatorSet animatorFullSet = new AnimatorSet();
-	        animatorFullSet.playTogether(imageCloseAnimatorSet, textCloseAnimatorSet);
-	        animatorFullSet.setDuration(1000);
-//	        animatorFullSet.addListener(mCloseOpenAnimatorListener);
-	        animatorFullSet.setStartDelay(0);
-//	        animatorFullSet.setInterpolator(new HesitateInterpolator());
-	        return animatorFullSet;
-	    }
-	
-	private RelativeLayout getImageWrapper(){
-		RelativeLayout imageWrapper = new RelativeLayout(getContext());
-		LinearLayout.LayoutParams imageWrapperLayoutParams = new LinearLayout.LayoutParams(menuItemSize, menuItemSize);
-        imageWrapper.setLayoutParams(imageWrapperLayoutParams);
-//        imageWrapper.setBackgroundColor(mContext.getResources().getColor(R.color.menu_item_background));
-//        imageWrapper.setOnClickListener(onCLick);
-        imageWrapper.addView(getItemImageButton());
-//        imageWrapper.addView(getDivider(context));
-        return imageWrapper;
+		AnimatorSet animatorFullSet = new AnimatorSet();
+		animatorFullSet.play(imageCloseAnimatorSet);
+		// animatorFullSet.playTogether(imageCloseAnimatorSet,
+		// textCloseAnimatorSet);
+		animatorFullSet.setDuration(1000);
+		// animatorFullSet.addListener(mCloseOpenAnimatorListener);
+		animatorFullSet.setStartDelay(0);
+		// animatorFullSet.setInterpolator(new HesitateInterpolator());
+		return animatorFullSet;
 	}
-	public ImageView getItemImageButton() {
-        ImageView imageView = new ImageButton(mContext);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
-        imageView.setLayoutParams(lp);
-//        imageView.setPadding((int) context.getResources().getDimension(R.dimen.menu_item_padding),
-//                (int) context.getResources().getDimension(R.dimen.menu_item_padding),
-//                (int) context.getResources().getDimension(R.dimen.menu_item_padding),
-//                (int) context.getResources().getDimension(R.dimen.menu_item_padding));
-        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        imageView.setBackgroundColor(mContext.getResources().getColor(android.R.color.transparent));
-        imageView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.icn_1));
-        imageView.setClickable(false);
-        imageView.setFocusable(false);
-        return imageView;
-    }
-	
+
+	private void fillOpenAnimations(List<Animator> imageAnimations,
+			int wrapperPosition) {
+		Animator imageRotation = wrapperPosition == 0 ? AnimatorUtils
+				.rotationOpenFromRight(getChildAt(wrapperPosition))
+				: AnimatorUtils
+						.rotationOpenVertical(getChildAt(wrapperPosition));
+		imageAnimations.add(imageRotation);
+	}
+
+	private void fillCloseAnimations(List<Animator> imageAnimations,
+			int wrapperPosition) {
+		Animator imageRotation = wrapperPosition == 0 ? AnimatorUtils
+				.rotationCloseToRight(getChildAt(wrapperPosition))
+				: AnimatorUtils
+						.rotationCloseVertical(getChildAt(wrapperPosition));
+		imageAnimations.add(imageRotation);
+	}
+
 	private void resetAnimations() {
 		int count = getChildCount();
-        for (int i = 0; i < count; i++) {
-            if (i == 0) {
-                resetSideAnimation(getChildAt(i));
-            } else {
-                resetVerticalAnimation(getChildAt(i));
-            }
-        }
-    }
-	
+		for (int i = 0; i < count; i++) {
+			if (i == 0) {
+				resetSideAnimation(getChildAt(i));
+			} else {
+				resetVerticalAnimation(getChildAt(i));
+			}
+		}
+	}
+
 	private void resetVerticalAnimation(View view) {
+		view.setRotation(0);
+		view.setRotationX(-90);
+		view.setRotationY(0);
+		view.setPivotX(menuItemSize / 2);
+		view.setPivotY(0);
+	}
+
+	/**
+	 * Set starting params to side animations
+	 */
+	private void resetSideAnimation(View view) {
 		view.setRotation(0);
 		view.setRotationX(0);
 		view.setRotationY(-90);
-		view.setPivotX(menuItemSize/2);
-		view.setPivotY(0);
-    }
-
-    /**
-     * Set starting params to side animations
-     */
-    private void resetSideAnimation(View view) {
-    	view.setRotation(0);
-		view.setRotationX(0);
-		view.setRotationY(-90);
 		view.setPivotX(menuItemSize);
-		view.setPivotY(menuItemSize/2);
-    }
+		view.setPivotY(menuItemSize / 2);
+	}
 
 }
